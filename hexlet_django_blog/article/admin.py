7bc1abf6064ac3bab050ab4496186aa9c7c95e95
Diana <dianaserik0@gmail.com>
2026-03-18 15:18:13 +0300

    Django Admin

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
-# Register your models here.
+from .models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "created_at",
+    )  # Перечисляем поля, отображаемые в таблице списка статей
+    search_fields = ["name", "body"]
+    list_filter = (
+        ("created_at", DateFieldListFilter),
+    )  # Перечисляем поля для фильтрации
