fa21de5985839106ca4a1edac285ef321df800fa
Diana <dianaserik0@gmail.com>
2026-03-20 15:05:07 +0300

    update (CRUD)

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from hexlet_django_blog.article import views
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView, ArticleFormEditView
 
 urlpatterns = [
-    path("", IndexView.as_view()),
+    path("", IndexView.as_view(), name='articles'),
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"),
     path("<int:id>/", ArticleView.as_view(), name='article_detail'),
-    # path('<int:article_id>/comments/<int:id>/', ArticleCommentsFormView.as_view()),
     path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
+    # path('<int:article_id>/comments/<int:id>/', ArticleCommentsFormView.as_view()),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views import View
 from hexlet_django_blog.article.models import Article, ArticleComment
@@ -15,8 +15,16 @@ class IndexView(View):
             },
         )
 
-    # def get(self, request, *args, **kwargs):
-    #     return HttpResponse("Hello, World!")
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs["id"])
+        return render(
+            request,
+            "articles/show.html",
+            context={
+                "article": article,
+            },
+        )
 
 class ArticleFormCreateView(View):
     def get(self, request, *args, **kwargs):
@@ -31,16 +39,26 @@ class ArticleFormCreateView(View):
         # Если данные некорректные, то возвращаем человека обратно на страницу с заполненной формой
         return render(request, 'articles/create.html', {'form': form})
 
-class ArticleView(View):
+class ArticleFormEditView(View):
     def get(self, request, *args, **kwargs):
-        article = get_object_or_404(Article, id=kwargs["id"])
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
         return render(
-            request,
-            "articles/show.html",
-            context={
-                "article": article,
-            },
+            request, "articles/update.html", {"form": form, "article_id": article_id}
         )
+    
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect("articles")
+
+        return render(
+            request, "articles/update.html", {"form": form, "article_id": article_id}
+    )
 
 class ArticleCommentFormView(View):
     def post(self, request, *args, **kwargs):
@@ -51,9 +69,9 @@ class ArticleCommentFormView(View):
             comment.content = check_for_spam(form.data["content"])
             comment.save()
 
-# def article(request, tags, article_id):
-#     return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
-    
+# def get(self, request, *args, **kwargs):
+#     return HttpResponse("Hello, World!")
+
 # def index(request):
 #     return render(
 #         request,
@@ -64,3 +82,6 @@ class ArticleCommentFormView(View):
 #         }
 #     )
 
+# def article(request, tags, article_id):
+#     return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
+    
